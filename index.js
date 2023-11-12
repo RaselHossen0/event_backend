c8914adfee43a36e75fc0894cda9d6d3d925f06b
Md. Rasel Hossen <hrasel2002@gmail.com>
2023-11-12 17:08:28 +0600

    Event Management Backend

--- index.js
+++ index.js
@@ -5,11 +5,7 @@ const app=express()
 app.listen(8800,()=>{
     console.log('server connected');
 })
-const dbConfig = {
-    user: 's2020015668',
-    password: 'db2023',
-    connectString: '103.221.254.51:1521/pdbdbms2023.cse.du.ac.bd', // e.g., 'remote-oracle-server:1521/your_service_name'
-  };
+
   oracledb.getConnection(dbConfig, (err, connection) => {
     if (err) {
       console.error('Error connecting to Oracle:', err);
